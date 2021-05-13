540b8c9aeba6af691bb705b002ead19f2306d16c
Joshua Andrei Nicolai Dotillos <joshua.dotillos.20@usjr.edu.ph>
2021-05-13 16:00:22 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+var num1;
+var num2;
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+var subtract = function (num1, num2) {
+    return num1 - num2;
+};
+num1 = 5;
+num2 = 7;
+console.log(add(num1, num2));
---
+++ index.ts
@@ -0,0 +1,15 @@
+let num1: number
+let num2: number
+
+const add = (num1:number, num2:number):number => {
+    return num1 + num2
+}
+
+const subtract = (num1:number, num2:number):number => {
+    return num1 - num2
+}
+
+num1 = 5
+num2 = 7
+
+console.log(add(num1, num2))
